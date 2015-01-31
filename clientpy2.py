d7fdaabe5109c4572c0ac18c8a209a65e3031678
Nishil <nishil94@gmail.com>
2015-01-30 20:46:57 -0600

    Added client file

---
+++ clientpy2.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+    
+def run(*commands):
+    HOST, PORT = "codebb.cloudapp.net", 17429
+    
+    data= "dnt" + " " + "nishilsucks" + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
+
+def subscribe():
+    HOST, PORT = "codebb.cloudapp.net", 17429
+    
+    data= "dnt" + " " + "nishilsucks" + "\nSUBSCRIBE\n"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        sock.connect((HOST, PORT))
+        sock.sendall(data)
+        sfile = sock.makefile()
+        rline = sfile.readline()
+        while rline:
+            print(rline.strip())
+            rline = sfile.readline()
+    finally:
+        sock.close()
