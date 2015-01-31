0530b89baaefd6ad3f81ac66db542acc71d8de99
Nishil <nishil94@gmail.com>
2015-01-30 21:59:15 -0600

    Added initialization

--- clientpy2.py
+++ clientpy2.py
@@ -5,7 +5,7 @@ def run(*commands):
     HOST, PORT = "codebb.cloudapp.net", 17429
     
     data= "dnt" + " " + "nishilsucks" + "\n" + "\n".join(commands) + "\nCLOSE_CONNECTION\n"
-
+    string = ""
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -14,10 +14,11 @@ def run(*commands):
         sfile = sock.makefile()
         rline = sfile.readline()
         while rline:
-            print(rline.strip())
+            string += rline.strip()
             rline = sfile.readline()
     finally:
         sock.close()
+    return string;
 
 def subscribe():
     HOST, PORT = "codebb.cloudapp.net", 17429
@@ -36,3 +37,34 @@ def subscribe():
             rline = sfile.readline()
     finally:
         sock.close()
+
+def init():
+    raw_data = run("SECURITIES")
+    data = raw_data.split(" ")
+    i = 0
+    tickerupdate = ""
+    companies = [];
+    while i < len(data):
+        if i == 0:
+            i-=3
+        else:
+            name = data[i]
+            net = float(data[i+1])
+            ratio = float(data[i+2])
+            volatility = float(data[i+3])
+            companies.append(Company(name, net, ratio, volatility))
+        i+=4
+    print companies
+class Company:
+    name = ""
+    net = 0.0
+    ratio = 0.0
+    volatility = 0.0
+    def __init__(self, n, ne, r, v):
+        self.name = n
+        self.net = ne
+        self.ratio = r
+        self.volatility = v
+
+
+    
